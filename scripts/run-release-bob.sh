019d1785ed2854eba4c7fb809b2adc949be390d8
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-11-23 18:17:24 +0800

    add restart node scripts

---
+++ scripts/run-release-bob.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+cargo run --release purge-chain --base-path /tmp/bob -y
+
+cargo run --release \-- \
+  --base-path /tmp/bob \
+  --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/QmRpheLN4JWdAnY7HGJfWFNbfkQCb6tFf4vvA6hgjMZKrR \
+  --chain=local \
+  --bob \
+  --port 30334 \
+  --telemetry-url ws://telemetry.polkadot.io:1024 \
+  --validator
