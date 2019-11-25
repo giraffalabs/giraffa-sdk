94f15ab01dfa8c822fb0065047e6bc3793b80be2
Anakorn Kyavatanakij <anakornk@gmail.com>
2019-11-25 21:15:17 +0800

    add content and links template

---
+++ modules/links/src/lib.rs
@@ -0,0 +1,30 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use support::{decl_module, decl_storage, decl_event, dispatch::Result};
+use system::ensure_signed;
+
+pub trait Trait: system::Trait {
+	/// The overarching event type.
+	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+}
+
+decl_storage! {
+	trait Store for Module<T: Trait> as Links {
+		
+	}
+}
+
+decl_module! {
+	/// The module declaration.
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+
+		fn deposit_event() = default;
+
+	}
+}
+
+decl_event!(
+	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
+		SomethingStored(u32, AccountId),
+	}
+);
